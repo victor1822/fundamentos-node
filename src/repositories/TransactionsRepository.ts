aec4e20a8da1580cddede87a2570e025fbe69215
Victor <victsales@gmail.com>
2020-08-27 08:38:59 -0300

    Primeiro commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: string;
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter((transaction: Transaction) => {
+        return transaction.type === 'income';
+      })
+      .map((transaction: Transaction) => transaction.value)
+      .reduce((acc: number, cur: number) => {
+        return acc + cur;
+      });
+    const outcome = this.transactions
+      .filter((transaction: Transaction) => {
+        return transaction.type === 'outcome';
+      })
+      .map((transaction: Transaction) => transaction.value)
+      .reduce((acc: number, cur: number) => {
+        return acc + cur;
+      });
+    const balance = income - outcome;
+
+    return {
+      income,
+      outcome,
+      balance,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title, value, type});
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: string;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,7 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: RequestDTO): Transaction {
   }
 }
 
